9526b394bdfaf1b66d7c5cf21f252eae1aa6fa2b
TheoKant <kantartzistheophilos@gmail.com>
2021-01-06 16:09:41 +0200

    emd complete

--- emd.py
+++ emd.py
@@ -71,107 +71,125 @@ def create_array_of_image( n , img ):
         dist_top_sum += n
     return img_array
 
-
-
-# train_array = list()
-# for i in range(60000):
-#     train_array.append(create_array_of_image( 7 , train_images[i] ))
-
-
-# for i in range(60000):
-#     sum1 = 0
-#     for j in range(16):
-#         sum1 += train_array[i][j][1]
-#     if sum1 < 100 :
-#         train_array[i][0][1] += 100 - sum1
-#     elif sum1 > 100 :
-#         diff = sum1 - 100
-#         counter = 0
-#         while diff > 0:
-#             if train_array[i][counter][1] > 0 :
-#                 train_array[i][counter][1] -= 1
-#                 diff -= 1
-#             counter += 1
-#             if counter == 16:
-#                 counter = 0
-
-arr1 = create_array_of_image( 7 , train_images[1] )
-arr2 = create_array_of_image( 7 , train_images[2] )
-
-
-sum1 = 0
-for j in range(16):
-    sum1 += arr1[j][1]
-if sum1 < 100 :
-    arr1[0][1] += 100 - sum1
-elif sum1 > 100 :
-    diff = sum1 - 100
-    counter = 0
-    while diff > 0:
-        if arr1[counter][1] > 0 :
-            arr1[counter][1] -= 1
-            diff -= 1
-        counter += 1
-        if counter == 16:
-            counter = 0
-
-sum1 = 0
-for j in range(16):
-    sum1 += arr2[j][1]
-if sum1 < 100 :
-    arr2[0][1] += 100 - sum1
-elif sum1 > 100 :
-    diff = sum1 - 100
-    counter = 0
-    while diff > 0:
-        if arr2[counter][1] > 0 :
-            arr2[counter][1] -= 1
-            diff -= 1
-        counter += 1
-        if counter == 16:
-            counter = 0
-
-#Add variables for flow capacity
-flows_in_clusters = list()
-for i in range(16):
-    flows = list()
-    for j in range(16):
-        x = LpVariable("f"+str(i)+"_"+str(j), 0 , arr1[i][1])
-        flows.append(x)
-    flows_in_clusters.append(flows)
-
 def euclidian( p1 , p2 ):
     return sqrt( pow(p1[0] - p2[0] , 2) + pow(p1[1] - p2[1] , 2) )
 
-distances = list()
-#Calculate distance for each flow
-for i in range(16):
-    for j in range(16):
-        distances.append( euclidian( arr1[i][0] , arr2[j][0] ) )
+def emd( n , arr1 , arr2 ):
+    total_clusters = pow( int(28 / n) , 2) 
+    #Add variables for flow capacity
+    flows_in_clusters = list()
+    for i in range(total_clusters):
+        flows = list()
+        for j in range(total_clusters):
+            x = LpVariable("f"+str(i)+"_"+str(j), 0 , arr1[i][1])
+            flows.append(x)
+        flows_in_clusters.append(flows)
+
+    distances = list()
+    #Calculate distance for each flow
+    for i in range(total_clusters):
+        for j in range(total_clusters):
+            distances.append( euclidian( arr1[i][0] , arr2[j][0] ) )
+
+    prob = LpProblem("myProblem", LpMinimize)
+
+    for j in range(total_clusters):
+        prob += lpSum([flows_in_clusters[j][i] for i in range(total_clusters)]) == arr1[j][1], "constr"+"_"+str(j)
+
+    for j in range(total_clusters):
+        prob += lpSum([flows_in_clusters[i][j] for i in range(total_clusters)]) == arr2[j][1], "constr2"+"_"+str(j)
+
+    final_sum = list()
+    for i in range(total_clusters):
+        for j in range(total_clusters):
+            final_sum.append(flows_in_clusters[i][j])
+
+    prob += lpSum([final_sum[i] * distances[i] for i in range(total_clusters*total_clusters)]) , "goal"
+
+    try:
+        prob.solve(PULP_CBC_CMD(msg=False))
+    except Exception:
+        print("problem with pulp")
+    return prob.objective.value()
 
 
-prob = LpProblem("myProblem", LpMinimize)
 
-# for i in range(16):
-#     prob += lpSum(clusters[i]) == arr1[i][1] 
+def find_10_closest( n , test_img , train_array ):
+    total = 0
+    ten_min = list()
+    ten_min_index = list()
+    for i in range(len(train_array)):
+        x = emd( n , test_img , train_array[i])
+        if total < 10:
+            ten_min.append(x)
+            ten_min_index.append(i)
+            ten_min, ten_min_index = (list(t) for t in zip(*sorted(zip(ten_min, ten_min_index))))
+            total += 1
+        else:
+            if x < ten_min[9]:
+                ten_min[9] = x 
+                ten_min_index[9] = i
+                ten_min, ten_min_index = (list(t) for t in zip(*sorted(zip(ten_min, ten_min_index))))
+    return ten_min_index
 
-for j in range(16):
-    prob += lpSum([flows_in_clusters[j][i] for i in range(16)]) == arr1[j][1], "constr"+"_"+str(j)
 
-for j in range(16):
-    prob += lpSum([flows_in_clusters[i][j] for i in range(16)]) == arr2[j][1], "constr2"+"_"+str(j)
-
-final_sum = list()
-for i in range(16):
-    for j in range(16):
-        final_sum.append(flows_in_clusters[i][j])
-
-prob += lpSum([final_sum[i] * distances[i] for i in range(16*16)]) , "goal"
+def test_no( o , n , test , train_array):
+    x = find_10_closest( n , test , train_array)
+    counter = 0
+    for i in range(len(x)):
+        if train_labels[x[i]] == test_labels[o]:
+            counter += 1
+    return counter
 
 
-try:
-     prob.solve()
-except Exception:
-     print("problem with pup")
+train_array_size = 100
+test_array_size = 10
+n = 4
+k = pow(int(28/n),2)
+train_array = list()
+for i in range(train_array_size):
+    train_array.append(create_array_of_image( n , train_images[i] ))
 
-print(prob.objective.value())
+test_array = list()
+for i in range(test_array_size):
+    test_array.append(create_array_of_image( n , test_images[i] ))
+
+for i in range(train_array_size):
+    sum1 = 0
+    for j in range(k):
+        sum1 += train_array[i][j][1]
+    if sum1 < 100 :
+        train_array[i][0][1] += 100 - sum1
+    elif sum1 > 100 :
+        diff = sum1 - 100
+        counter = 0
+        while diff > 0:
+            if train_array[i][counter][1] > 0 :
+                train_array[i][counter][1] -= 1
+                diff -= 1
+            counter += 1
+            if counter == k:
+                counter = 0
+
+for i in range(test_array_size):
+    sum1 = 0
+    for j in range(k):
+        sum1 += test_array[i][j][1]
+    if sum1 < 100 :
+        test_array[i][0][1] += 100 - sum1
+    elif sum1 > 100 :
+        diff = sum1 - 100
+        counter = 0
+        while diff > 0:
+            if test_array[i][counter][1] > 0 :
+                test_array[i][counter][1] -= 1
+                diff -= 1
+            counter += 1
+            if counter == k:
+                counter = 0
+
+sum = 0
+for i in range(len(test_array)):
+    sum += test_no( i , n , test_array[i] , train_array )
+
+print( sum / len(test_array) )
